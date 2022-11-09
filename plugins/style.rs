daa320cc639f547d48c35ef8a549ed0f0f79f7e8
flbr <benrhammond@gmail.com>
2022-11-08 19:01:58 -0800

    removes image backup dir and updates template

---
+++ plugins/style.rs
@@ -0,0 +1,25 @@
+use std::fs;
+use std::io;
+
+mod utils;
+use utils::text::*;
+
+fn main() {
+	let mut lines: Vec<String> = vec![];
+	let std_lines = io::stdin().lines();
+	for l in std_lines {
+		lines.push(l.unwrap());
+	}
+
+	for l in &lines {
+		if l.contains("{{style}}") {
+			// eventually pull the style path from config?
+			match fs::read_to_string("docs/style.css") {
+				Ok(css) => println!("{}", replace(&l, "{{style}}", &css)),
+				Err(_) => (),
+			}
+		} else {
+			println!("{}", l);
+		}
+	}
+}
