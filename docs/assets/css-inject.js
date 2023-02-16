ee1af80c117e12fd7903ff011b83a42d93784679
flbr <benrhammond@gmail.com>
2023-02-15 19:56:40 -0800

    misc additions
    Former-commit-id: 7f7c5f2618ddb68f69738a8969f48e40c2db4157

---
+++ docs/assets/css-inject.js
@@ -0,0 +1,6 @@
+var head = document.getElementsByTagName('HEAD')[0];
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = '../assets/prism.css';
+head.appendChild(link);
