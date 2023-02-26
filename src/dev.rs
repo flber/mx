16333f1b505cca4d0ca35af1e7fd7a5050799dfd
flbr <benrhammond@gmail.com>
2023-02-26 15:11:55 -0800

    test dev login

---
+++ src/dev.rs
@@ -0,0 +1,95 @@
+use rocket::form::Form;
+use rocket::http::{Cookie, CookieJar};
+use rocket::outcome::IntoOutcome;
+use rocket::request::{self, FlashMessage, FromRequest, Request};
+use rocket::response::{Flash, Redirect};
+
+use rocket_dyn_templates::{context, Template};
+
+use std::env;
+
+#[derive(FromForm)]
+struct Login<'r> {
+	username: &'r str,
+	password: &'r str,
+}
+
+#[derive(Debug)]
+struct User(usize);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for User {
+	type Error = std::convert::Infallible;
+
+	async fn from_request(request: &'r Request<'_>) -> request::Outcome<User, Self::Error> {
+		request
+			.cookies()
+			.get_private("user_id")
+			.and_then(|cookie| cookie.value().parse().ok())
+			.map(User)
+			.or_forward(())
+	}
+}
+
+#[macro_export]
+macro_rules! session_uri {
+    ($($t:tt)*) => (rocket::uri!("/dev", $crate::dev:: $($t)*))
+}
+
+pub use session_uri as uri;
+
+#[get("/")]
+fn index(user: User) -> Template {
+	Template::render(
+		"session",
+		context! {
+			user_id: user.0,
+		},
+	)
+}
+
+#[get("/", rank = 2)]
+fn no_auth_index() -> Redirect {
+	Redirect::to(uri!(login_page))
+}
+
+#[get("/login")]
+fn login(_user: User) -> Redirect {
+	Redirect::to(uri!(index))
+}
+
+#[get("/login", rank = 2)]
+fn login_page(flash: Option<FlashMessage<'_>>) -> Template {
+	Template::render("login", &flash)
+}
+
+#[post("/login", data = "<login>")]
+fn post_login(jar: &CookieJar<'_>, login: Form<Login<'_>>) -> Result<Redirect, Flash<Redirect>> {
+	match (env::var("USER"), env::var("PASSWORD")) {
+		(Ok(user), Ok(pw)) => {
+			if login.username == user && login.password == pw {
+				jar.add_private(Cookie::new("user_id", 1.to_string()));
+				Ok(Redirect::to(uri!(index)))
+			} else {
+				Err(Flash::error(
+					Redirect::to(uri!(login_page)),
+					"Invalid username/password.",
+				))
+			}
+		}
+		_ => Err(Flash::error(
+			Redirect::to(uri!(login_page)),
+			"Failed to load environment variables.",
+		)),
+	}
+}
+
+#[post("/logout")]
+fn logout(jar: &CookieJar<'_>) -> Flash<Redirect> {
+	jar.remove_private(Cookie::named("user_id"));
+	Flash::success(Redirect::to(uri!(login_page)), "Successfully logged out.")
+}
+
+pub fn routes() -> Vec<rocket::Route> {
+	routes![index, no_auth_index, login, login_page, post_login, logout]
+}
--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,39 @@ extern crate rocket;
 use rocket::fs::FileServer;
 use rocket::response::content::RawJson;
 use rocket::State;
-use std::net::{SocketAddr, /*IpAddr*/};
+use rocket_dyn_templates::Template;
+use std::net::SocketAddr;
 
 // use std::sync::Mutex;
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::env;
+
+mod dev;
 
 struct UserCount(AtomicUsize);
 // struct UserIPs(Vec<Mutex<IpAddr>>);
 
 #[get("/count")]
 fn count(user_count: &State<UserCount>) -> RawJson<String> {
-    RawJson(format!(
-        "{{\"user_count\": {}}}",
-        user_count.0.load(Ordering::Relaxed)
-    ))
+	RawJson(format!(
+		"{{\"user_count\": {}}}",
+		user_count.0.load(Ordering::Relaxed)
+	))
 }
 
 #[post("/count")]
 fn inc_count(user_count: &State<UserCount>, sock: SocketAddr) {
-    user_count.0.fetch_add(1, Ordering::SeqCst);
-    println!("Remote Address: {:?}", sock.ip());
+	user_count.0.fetch_add(1, Ordering::SeqCst);
+	println!("Remote Address: {:?}", sock.ip());
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .manage(UserCount(AtomicUsize::new(0)))
-        // .manage(UserIPs(vec![]))
-        .mount("/", FileServer::from("public/"))
-        .mount("/api", routes![count, inc_count])
+	rocket::build()
+		.manage(UserCount(AtomicUsize::new(0)))
+		// .manage(UserIPs(vec![]))
+		.attach(Template::fairing())
+		.mount("/dev", dev::routes())
+		.mount("/", FileServer::from("public/"))
+		.mount("/api", routes![count, inc_count])
 }
